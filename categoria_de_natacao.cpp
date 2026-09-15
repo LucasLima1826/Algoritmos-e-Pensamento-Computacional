8520068158532957506eaa8a2e25ed2262ab1d68
Lucas Lima <lucaslimaolft@gmail.com>
2026-09-15 10:09:40 -0300

    Update categoria_de_natacao.cpp

--- categoria_de_natacao.cpp
+++ categoria_de_natacao.cpp
@@ -9,16 +9,16 @@ int main(void){
 	if(idade > 4 && idade <= 7){
 		printf("Voce e da categoria pre-mirim");
 	}
-	else if(idade > 7 && idade <= 10){
+	else if(idade <= 10){
 		printf("Voce e da categoria mirim");
 	}
-	else if(idade > 10 && idade <= 13){
+	else if(idade <= 13){
 		printf("Voce e da categoria infantil");
 	}
-	else if(idade >13 && idade <= 17){
+	else if(idade <= 17){
 		printf("Voce e da categoria infanto-juvenil");
 	}
-	else if(idade >17 && idade <= 20){
+	else if(idade <= 20){
 		printf("Voce e da categoria juvenil");
 	}
 	else if(idade>=21){
